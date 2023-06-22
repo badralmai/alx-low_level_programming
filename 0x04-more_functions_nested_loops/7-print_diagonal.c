157e0ce893ab38080b1b06e943579ee78df673d9
badralmai <badralmai1@gmail.com>
2023-06-22 16:54:13 -0700

    Tak 7 update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,12 +19,10 @@ void print_diagonal(int n)
 	{
 	for (x = 0; x < n; x++)
 	{
-	for (y = 0; y < n; y++)
+	for (y = 0; y < x; y++)
 	{
 	_putchar(32);
 	}
-	_putchar(92);
-	_putchar('\n');
-	}
-	}
+        _putchar('\n');
+        }
 }
