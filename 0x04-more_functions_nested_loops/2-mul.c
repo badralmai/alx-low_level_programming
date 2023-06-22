4fae806a6275a757308db84e45228d0e10845f3a
badralmai <badralmai1@gmail.com>
2023-06-22 15:42:53 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * nul - Multiplies two integres
+ * @x: The firt number to multiply
+ * @y: The second number to multyply with firt number
+ * Return: The result of multi between x param and y param
+ */
+
+int nul(int x, int y)
+{
+	return (x * y);
+}
